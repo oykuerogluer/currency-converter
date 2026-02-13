babc9a4feb7b390c1504ad7b1174095bbf7e3b39
Oyku Erogluer <oykuerogluer04@gmail.com>
2026-02-14 02:18:41 +0300

    Fixed codes

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
-    public static double getLiveRate(String currency) {
-        if (currency.equals("USD")) return 50.0;
-        if (currency.equals("EUR")) return 55.0;
-        return 1.0;
-    }
+ public class Main {
+    //public static double getLiveRate(String currency) {
+        //if (currency.equals("USD")) return 44.0;
+        //if (currency.equals("EUR")) return 52.0;
+        //return 1.0;
+    //}
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -27,11 +27,11 @@ public class Main {
         double result = 0;
 
         if (choice == 1) {
-            result = amount * 30.0;
+            result = amount * 44.0;
             System.out.println("Converted amount: " + result + " TRY");
         }
         else if (choice == 2) {
-            result = amount * 33.0;
+            result = amount * 52.0;
             System.out.println("Converted amount: " + result + " TRY");
         }
         else {
